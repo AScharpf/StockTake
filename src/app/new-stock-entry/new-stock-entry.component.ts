e83398330d1b6388c84bf2746669548d75cd8a82
Alexander Scharpf <alexander.scharpf94@gmx.de>
2023-03-05 09:01:14 +0100

    Add: New Stock Entry Form

---
+++ src/app/new-stock-entry/new-stock-entry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-stock-entry',
+  templateUrl: './new-stock-entry.component.html',
+  styleUrls: ['./new-stock-entry.component.css']
+})
+export class NewStockEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
