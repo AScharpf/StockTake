4de70e387ca84300929f9767dcd27ecd00caac13
Alexander Scharpf <alexander.scharpf94@gmx.de>
2023-03-10 22:13:36 +0100

    Add: Playing around with animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,10 +16,12 @@ import { OneStatisticComponent } from './one-statistic/one-statistic.component';
     LandingPageComponent,
     StatisticsComponent,
     OneStatisticComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { trigger, transition, style, state, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      state('visible', style({
+        opacity: '1',
+        color: 'white',
+      })),
+      transition('void => visible', [
+        animate('0.5s')
+      ]),
+    ])]
 })
+
+
 export class LandingPageComponent implements OnInit {
+state: any;
 
   constructor() { }
 
   ngOnInit(): void {
+  this.state='visible';
   }
+
+
+
 
 }
