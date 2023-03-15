6526c034ecfb4f2602827bba4de3daa105cfcaf2
Alexander Scharpf <alexander.scharpf94@gmx.de>
2023-03-16 00:01:09 +0100

    Add: Setting up server

--- server/router.js
+++ server/router.js
@@ -1,6 +1,7 @@
 'use strict'
 const router = require('express').Router();
-const controller = require('./controllers');
-router.get('/endpoint', controller.getFunction);
-router.post('/endpoint', controller.postFunction);
+const controller = require('./controller');
+router.get('/', (req, res) => {
+  res.send('<h1>hello world</h1>')});
+// router.post('/endpoint', controller.postFunction);
 module.exports = router;
